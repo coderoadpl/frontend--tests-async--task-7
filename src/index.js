60472075f1523f874e712cef37210aa098180b3e
Mateusz Choma <kontakt@coderoad.pl>
2021-04-07 19:18:09 +0200

    Fetching users

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log('Hello CodeRoad!')
+const fetchUsers = () => {
+    return fetch('https://randomuser.me/api?results=3&inc=email,name')
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+            return data.results.map((result) => {
+                return {
+                    email: result.email,
+                    firstName: result.name.first,
+                    lastName: result.name.last,
+                }
+            })
+        })
+}
+
+fetchUsers().then((data) => {
+    console.log(data)
+})
